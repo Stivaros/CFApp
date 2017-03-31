d3a0b46a9967b2b8ad638796c1100823742a4bc0
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2017-03-31 21:19:29 +0100

    :construction: 4.3 Create orders controller, views and routes
    Manually create orders_controller.rb, index.html.erb and show.html.erb, update application.html.erb to show new orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   resources :products
+
+  resources :orders, only: [:index, :show, :create, :destroy]
+
   get '/about' => 'static_pages#about'
 
   get '/contact' => 'static_pages#contact'
