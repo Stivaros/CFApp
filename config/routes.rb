b891fa6d93ca7c68fe7817f74b0d82c6c7ff3390
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2017-03-31 21:09:26 +0100

    :art::construction: Put all files in div containers
    Place a container div around the yield in the application view, therefore not having to type it within each new view.

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   get '/index' => 'static_pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
 end
