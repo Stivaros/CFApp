d0dac12da5ce572b0f13d9f564d19f8b12cc803c
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2017-04-28 23:51:39 +0000

    :wrench::bug: Fix link not working in password reset email by setting defaulturl to localhost

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,5 +85,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   # Add host
-  config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net'} 
+  config.action_mailer.default_url_options = { :host => 'localhost:3000'} 
 end
