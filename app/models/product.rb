7afc898366ba0306f412c1c31be67b603b26693c
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2017-05-04 17:09:54 +0100

    :bento: Complete table associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :comments
 
   def self.search(search_term)
     Rails.env.production? ? @products = Product.where("name ilike ?", "%#{search_term}%") : @products = Product.where("name LIKE ?", "%#{search_term}%")
