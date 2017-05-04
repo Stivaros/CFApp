dfc6976876cdf555fd8c1bc9c50feee56cc6dd9c
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2017-05-04 18:18:49 +0100

    :art: Use scopes and methods to pull highest and lowest rated comment per product

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,12 @@ class Product < ApplicationRecord
   def self.search(search_term)
     Rails.env.production? ? @products = Product.where("name ilike ?", "%#{search_term}%") : @products = Product.where("name LIKE ?", "%#{search_term}%")
   end
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+  
+  def lowest_rating_comment
+    comments.rating_asc.first
+  end
 end
