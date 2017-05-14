fe955859d4cd1d892a42ed663770356735d47df9
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2017-05-14 22:49:12 +0100

    Revert ":lipstick: Allow admin to delete comments on product show page"
    This reverts commit 9db7c1cad34f51ee1e1ab4be1bed84dd7710cdca.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,17 +3,6 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-
-    alias_action :create, :update, :destroy, to: :cud
-
-    if user.admin?
-      can :manage, :all
-    else
-      can :read, :all
-      can :manage, User, id: user.id
-      can :create, Comment
-      cannot :cud, Product
-      cannot :destroy, Order
-    end
+    can :manage, User, id: user.id
   end
 end
