ade1715e0d6bbc6d01a389779f1e17f09ccd93d0
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2017-05-14 22:52:45 +0100

    :lock: Update user permissions across all classes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,17 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+
+    alias_action :create, :update, :destroy, to: :cud
+
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+      can :manage, User, id: user.id
+      can :create, Comment
+      cannot :cud, Product
+      cannot :destroy, Order
+    end
   end
 end
