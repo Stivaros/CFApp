dc1555cc0d6f3a1b4c95bc9dea3c69a583f0c493
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2017-05-18 12:33:05 +0100

    :bug::lock: Fix User Defined Abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,7 @@ class Ability
     user ||= User.new # guest user (not logged in)
 
     alias_action :create, :update, :destroy, to: :cud
+    alias_action :create, :update, to: :cu
 
     if user.admin?
       can :manage, :all
@@ -12,8 +13,8 @@ class Ability
       can :read, :all
       can :manage, User, id: user.id
       can :create, Comment
+      can :cu, Order, user_id: user.id
       cannot :cud, Product
-      cannot :destroy, Order
     end
   end
 end
