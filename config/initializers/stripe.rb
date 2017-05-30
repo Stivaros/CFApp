1f57600951292e6eca65f9152b7d9472911522de
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2017-05-30 14:50:36 +0100

    :wrench: Add Stripe initialisation

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,11 @@
+if Rails.env.production?
+  Rails.configuration.stripe = {
+    publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
+    secret_key: ENV['STRIPE_SECRET_KEY']
+  }
+else
+  Rails.configuration.stripe = {
+    publishable_key = 'pk_test_jl92GS20CTg61cqSabND6MT4',
+    secret_key = 'sk_test_qvlUMxroXZ9HTmhhPNDcny4M'
+  }
+end
