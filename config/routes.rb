864b3bcca2c8e394e26dbb32daf92f3a37e26a99
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2017-06-01 11:32:20 +0100

    :wrench: Add POST route for payment creation

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
 
   get '/index' => 'static_pages#index'
 
+  post 'payments/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#landing_page'
 end
