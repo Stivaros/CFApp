4766af8bb3a2022f2c3c5f5d7504962b5b9fec85
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2017-06-01 12:03:20 +0100

    :zap: Add create method to payment controller
    This method allows payments to be made via Stripe's API. Successful charges are created as orders in the DB.
    
    Using 'receipt_email' the user is also sent an email

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,2 +1,31 @@
 class PaymentsController < ApplicationController
+
+  def create
+    @product = Product.find(params[:product_id])
+    @user = current_user
+    token = params[:stripeToken]
+    begin
+      charge = Stripe::Charge.create(
+        amount: (@product.price * 100).to_i,
+        currency: "gbp",
+        source: token,
+        description: params[:stripeEmail],
+        receipt_email: @user.email
+      )
+
+      if charge.paid
+        Order.create(
+          user_id: @user.id,
+          product_id: params[:product_id],
+          total: @product.price
+        )
+        flash[:notice] = "Thank you for purchasing #{@product.name}"
+      end
+    rescue Stripe::CardError => e
+      body = e.json_body
+      err = body[:error]
+      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+    end
+    redirect_to product_path(@product)
+  end
 end
